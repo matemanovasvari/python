07a7737162f8922aaab6ce864c9cdb7b204f54d3
matemanovasvari <nagy.mano.mate@gmail.com>
2022-11-24 13:12:49 +0100

    22.11.24.

---
+++ 06 - Elágazások (switch-case)/Feladat 05/main.py
@@ -0,0 +1,11 @@
+resistor1: int = None
+resistor2: int = None
+parallel: int = None
+series: int = None
+
+
+print("Kérem az első ellenállás értékét!")
+resistor1 = int(input())
+print("Kérem a második ellenállás értékét!")
+resistor2 = int(input())
+
---
+++ 07 - For Ciklus/Feladat 01/main.py
@@ -0,0 +1,2 @@
+for i in range(2, 15+1):
+    print(i)
